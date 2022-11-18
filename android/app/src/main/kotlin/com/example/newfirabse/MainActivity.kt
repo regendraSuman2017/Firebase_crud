bf027333f1d7120d8a8953957797bfb8fd040d5c
pravesh5428 <pravesh@ubitechsolutions.com>
2022-11-18 15:08:27 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/newfirabse/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.newfirabse
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/insertData.dart
@@ -0,0 +1,156 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class InsertData extends StatefulWidget {
+  const InsertData({Key? key}) : super(key: key);
+
+  @override
+  State<InsertData> createState() => _InsertDataState();
+}
+
+class _InsertDataState extends State<InsertData> {
+
+  CollectionReference students =
+  FirebaseFirestore.instance.collection('students');
+
+  final _formKey = GlobalKey<FormState>();
+
+
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+
+  var name = "";
+  var email = "";
+  var password = "";
+
+  clearText() {
+    nameController.clear();
+    emailController.clear();
+    passwordController.clear();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:  Form(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+          child: ListView(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0),
+                child: TextFormField(
+                  autofocus: false,
+                  decoration: InputDecoration(
+                    labelText: 'Name: ',
+                    labelStyle: TextStyle(fontSize: 20.0),
+                    border: OutlineInputBorder(),
+                    errorStyle:
+                    TextStyle(color: Colors.redAccent, fontSize: 15),
+                  ),
+                  controller: nameController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter Name';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0),
+                child: TextFormField(
+                  autofocus: false,
+                  decoration: InputDecoration(
+                    labelText: 'Email: ',
+                    labelStyle: TextStyle(fontSize: 20.0),
+                    border: OutlineInputBorder(),
+                    errorStyle:
+                    TextStyle(color: Colors.redAccent, fontSize: 15),
+                  ),
+                  controller: emailController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter Email';
+                    } else if (!value.contains('@')) {
+                      return 'Please Enter Valid Email';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0),
+                child: TextFormField(
+                  autofocus: false,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password: ',
+                    labelStyle: TextStyle(fontSize: 20.0),
+                    border: OutlineInputBorder(),
+                    errorStyle:
+                    TextStyle(color: Colors.redAccent, fontSize: 15),
+                  ),
+                  controller: passwordController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter Password';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Validate returns true if the form is valid, otherwise false.
+                        if (_formKey.currentState!.validate()) {
+                          setState(() {
+                            name = nameController.text;
+                            email = emailController.text;
+                            password = passwordController.text;
+                            addUser();
+                            clearText();
+                          });
+                        }
+                      },
+                      child: Text(
+                        'Register',
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => {clearText()},
+                      child: Text(
+                        'Reset',
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                      style: ElevatedButton.styleFrom(primary: Colors.blueGrey),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> addUser() {
+    return students
+        .add({
+      'name': name,
+      'email': email,
+      'password': password})
+        .then((value) => print('User Added'))
+        .catchError((error) => print('Failed to Add user: $error'));
+  }
+}
